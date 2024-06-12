97c12990b7e0478822665584ab67a3d470f55a68
Rodrigo froes <craftland262@gamil.com>
2024-06-11 21:48:09 -0300

    Feat: Atualização

--- routes/pagamentoRoute.js
+++ routes/pagamentoRoute.js
@@ -4,5 +4,8 @@ const pagamentoController = require('../controllers/pagamentoController');
 const router = express.Router();
 let ctrl = new pagamentoController();
 router.get('/', ctrl.screenPagamento);
+router.get('/pagamentoTrinta', ctrl.pagamentoTrinta);
+router.get('/pagamentoCinquenta', ctrl.pagamentoCinquenta);
+router.get('/pagamentoOutros', ctrl.pagamenotoOutros);
 
 module.exports = router;
